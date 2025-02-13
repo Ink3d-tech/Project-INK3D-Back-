5b21f912e4facf95c5da6c17ba7782cf26042fc8
Gino-PE <gino.pacheco@gmail.com>
2025-02-13 14:07:12 -0300

    set up, dependencias, entidades, rolesguard, singUp y singIn

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,52 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity.js';
+import { Repository } from 'typeorm';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async signUp(user: Partial<User>) {
+    const existingUser = await this.userRepository.findOne({
+      where: { email: user.email },
+    });
+    if (existingUser) {
+      throw new BadRequestException('Email already in use');
+    }
+
+    const hashedPassord = await bcrypt.hash(user.password, 10);
+
+    const newUser = { ...user, password: hashedPassord };
+    if (!newUser) {
+      throw new NotFoundException('User not found');
+    }
+    const savedUser = await this.userRepository.save(newUser);
+    return savedUser;
+  }
+
+  async signIn(email: string, password: string) {
+    const user = await this.userRepository.findOne({ where: { email } });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      throw new NotFoundException('Invalid credentials');
+    }
+    const payload = { userId: user.id, email: user.email };
+
+    const token = this.jwtService.sign(payload);
+    return { token, message: 'User logged in successfully' };
+  }
+}
