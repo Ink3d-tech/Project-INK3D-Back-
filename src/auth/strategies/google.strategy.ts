2cdd25c7f9fc894a9f946a32ca9d0e3ec33eb548
ariel1092 <fede_10_92.com>
2025-02-22 19:44:51 -0300

    configuracion para render

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -9,7 +9,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: configService.get<string>('GOOGLE_CLIENT_ID'),
       clientSecret: configService.get<string>('GOOGLE_CLIENT_SECRET'),
-      callbackURL: 'https://project-ink3d-back-1.onrender.com/auth/google/callback',
+      callbackURL: configService.get<string>('GOOGLE_CALLBACK_URL'),
       scope: ['email', 'profile'],
     });
   }
