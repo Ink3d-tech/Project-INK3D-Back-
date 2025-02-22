c307e1ec768be4a4b6eead3fa40b79ff2ed1da32
ariel1092 <fede_10_92.com>
2025-02-22 19:27:09 -0300

    configuracion para render

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -60,7 +60,7 @@ export class AuthController {
   @ApiBody({
     description: 'User credentials for login',
     examples: {
-      'user.signin': { // <- Cambio aquí
+      'user.signin': {
         value: {
           email: 'john@example.com',
           password: '123456789',
@@ -72,22 +72,21 @@ export class AuthController {
     return this.authService.signIn(credentials.email, credentials.password);
   }
 
-@UseGuards(GoogleAuthGuard)
-@Get('google')
-async googleLogin() {
-  return { message: 'Redirecting to Google login...' };
-}
+  @UseGuards(GoogleAuthGuard)
+  @Get('google')
+  async googleLogin() {
+    return { message: 'Redirecting to Google login...' };
+  }
 
-  
   @UseGuards(GoogleAuthGuard)
   @Get('google/callback')
-  async googleCallback(@Req() req) {
+  async googleCallback(@Req() req, @Res() res) {
     try {
       const response = await this.authService.signInWithGoogle(req.user);
-      return response; // Devuelve el token directamente en la respuesta
+      return res.redirect(`https://www.youtube.com/?token=${response.access_token}`);
     } catch (error) {
       console.error('Google login error:', error);
-      throw new Error('Error en la autenticación con Google');
+      return res.redirect('https://www.youtube.com/?error=auth_failed');
     }
   }
 }
