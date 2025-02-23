3b3fe672519b9e73218d8d74ef806b6a4cfbfc2b
Gino-PE <gino.pacheco@gmail.com>
2025-02-23 19:56:24 -0300

    google strategy import FIXED, payment methods, CRUD orders y CRUD discount TESTING PENDING

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,7 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import googleOauthConfg from 'src/config/google-oauth.config';
 import { ConfigModule } from '@nestjs/config';
-import { GoogleStrategy } from 'src/strategy/google.strategy';
+import { GoogleStrategy } from './strategies/google.strategy';
 
 @Module({
   imports: [
