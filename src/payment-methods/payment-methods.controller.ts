af8e1c65bd59ac641b4aa74b63fa080decd8ceeb
Gino-PE <gino.pacheco@gmail.com>
2025-02-24 17:52:27 -0300

    Payment Method fixed front testing pending

--- src/payment-methods/payment-methods.controller.ts
+++ src/payment-methods/payment-methods.controller.ts
@@ -2,12 +2,42 @@ import { Controller, Post, Body, Req } from '@nestjs/common';
 import { PaymentMethodsService } from './payment-methods.service';
 import { Product } from '../entities/product.entity';
 import { Request } from 'express';
+import { ApiBody, ApiOperation } from '@nestjs/swagger';
+import { PaymentMethodDto } from './dto/payment-method.dto';
 
 @Controller('payment-methods')
 export class PaymentMethodsController {
   constructor(private readonly paymentMethodsService: PaymentMethodsService) {}
 
   @Post('create')
+  @ApiOperation({ summary: 'Crear un pago' })
+  @ApiBody({
+    type: PaymentMethodDto,
+    description: 'Datos del pago a crear',
+    required: true,
+    examples: {
+      'payment-method.create': {
+        value: {
+          orderId: 'cdaf3d44-8b81-4b41-8b3a-8c5e171d737c',
+          currency: 'USD',
+          products: [
+            {
+              id: 'a52a1076-e7ff-46af-9e92-c4300a8028bf',
+              title: 'Camiseta Negra',
+              price: 10,
+              quantity: 1,
+            },
+            {
+              id: '4feb09a5-a2c5-4d02-a4b3-d4cc08deb212',
+              title: 'Pantalón Jeans Azul',
+              price: 20,
+              quantity: 2,
+            },
+          ],
+        },
+      },
+    },
+  })
   createPayment(
     @Body()
     data: {
