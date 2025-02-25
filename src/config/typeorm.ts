edde7de129918b24ae50d3a4a7205059b5e2f4f5
ariel1092 <fede_10_92.com>
2025-02-25 10:14:57 -0300

    se agregaron las variables de entorno de la DB

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -3,19 +3,23 @@ import { config as dotenvConfig } from 'dotenv';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
 dotenvConfig({ path: './.env.development' });
+
 const config = {
   type: 'postgres',
   host: `${process.env.DB_HOST}`,
-  port: `${process.env.DB_PORT}`,
+  port: parseInt(`${process.env.DB_PORT}`, 10),  // Asegúrate de convertir a número
   username: `${process.env.DB_USER}`,
   password: `${process.env.DB_PASSWORD}`,
   database: `${process.env.DB_NAME}`,
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/**/*.migrations{js,ts}'],
   autoLoadEntities: true,
-  dropSchema: false,
+  dropSchema: true,
   synchronize: true,
   logging: true,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
 };
 
 export default registerAs('typeorm', () => config);
