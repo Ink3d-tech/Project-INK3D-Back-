d8cf08acdf135efeada9c97c814e4a06ee6ba283
Gino-PE <gino.pacheco@gmail.com>
2025-03-02 17:51:05 -0300

    notificacion email de compra realizada

--- src/payment-methods/payment-methods.module.ts
+++ src/payment-methods/payment-methods.module.ts
@@ -2,11 +2,16 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PaymentMethodsService } from './payment-methods.service';
 import { PaymentMethodsController } from './payment-methods.controller';
+import { NodemailerModule } from 'src/nodemailer/nodemailer.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrdersService } from 'src/orders/orders.service';
+import { Order } from 'src/entities/order.entity';
+import { NodeMailerService } from 'src/nodemailer/nodemailer.service';
 
 @Module({
   controllers: [PaymentMethodsController],
-  imports: [ConfigModule],
-  providers: [PaymentMethodsService],
+  imports: [ConfigModule, NodemailerModule, TypeOrmModule.forFeature([Order])],
+  providers: [PaymentMethodsService, NodeMailerService, OrdersService],
   exports: [PaymentMethodsService],
 })
 export class PaymentMethodsModule {}
