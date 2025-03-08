5a5a40f54af106d61e015430aa6078a600076c7a
Ink3d-tech <ink3dproject@gmail.com>
2025-03-08 18:47:55 -0300

    Update typeorm.ts

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -13,7 +13,7 @@ const config = {
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/**/*.migrations{js,ts}'],
   autoLoadEntities: true,
-  dropSchema: true,
+  dropSchema: false,
   synchronize: true,
   logging: false,
   ssl: false
